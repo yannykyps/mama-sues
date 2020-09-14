c3b395f6b3f29f99e858b208d17de40c18b86d97
YannyKyprianou <yannykyps@hotmail.com>
2020-09-14 12:10:26 +0100

    more changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
-import Layout from "../components/Layout"
+import LayoutCom from "../components/LayoutCom"
 import SEO from "../components/SEO"
 
 const Index = () => (
-  <Layout>
+  <LayoutCom>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Now go build something great.</p>
-  </Layout>
+  </LayoutCom>
 )
 
 export default Index
