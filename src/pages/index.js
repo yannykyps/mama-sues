bf61b772ad978e786fb20c47699adeacf8c1bd1c
YannyKyprianou <yannykyps@hotmail.com>
2020-09-14 11:16:00 +0100

    changes made

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
   </Layout>
