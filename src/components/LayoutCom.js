445495e2af20d73e5c438e7fed862434cf00492c
YannyKyprianou <yannykyps@hotmail.com>
2020-09-14 12:12:58 +0100

    new

---
+++ src/components/LayoutCom.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Test from "./Test"
+
+const LayoutCom = () => {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Test />
+    </>
+  )
+}
+
+export default LayoutCom
