9f9a56d93ed6fdcf51a95dfb913938a5a97c0f60
YannyKyprianou <yannykyps@hotmail.com>
2020-09-14 09:57:47 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
+require("dotenv").config({
+  path:`.env.${process.env.NODE_ENV}`
+})
+
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mama Sue's Cook House`,
+    description: `Mama Sue's Cook House. Gorgeous home cooked food`,
+    author: "Yanny Kyprianou",
+    twitterUsername: "@yannykyps",
+    image: "/twitter-img.png",
+    siteUrl: "https://www.mamasues.com",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +35,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `u85zep1fjtj4`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    },
   ],
 }
