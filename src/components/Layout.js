f2b47fe77479ab7303a15956b490cd265b6a5265
YannyKyprianou <yannykyps@hotmail.com>
2020-09-18 15:26:32 +0100

    update again

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
-// import CookieConsent from 'react-cookie-consent';
 
 
 const Layout = ({children}) => {
@@ -14,18 +13,9 @@ const Layout = ({children}) => {
     <div>
     <Navbar toggleSidebar={toggleSidebar}/>
     <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
-
     {children}
     </div>
   )
 }
 
 export default Layout
-
-    /* <CookieConsent
-          location="bottom"
-          buttonText="I understand"
-          declineButtonText="Decline"
-          cookieName="gatsby-gdpr-google-analytics">
-This site uses cookies to enhance the user experience
-</CookieConsent> */
