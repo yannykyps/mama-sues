17ed888df40916231b265d3a99c177bfa389b9a4
YannyKyprianou <yannykyps@hotmail.com>
2021-03-15 09:07:58 +0000

    noreferrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ const Footer = () => {
       </div>
     </div>
       <p>
-        &copy; {new Date().getFullYear()} Mama Sue's Cook House. Site by <a href="https://yannyweb.com/" target="_new">yannyweb.</a>
+        &copy; {new Date().getFullYear()} Mama Sue's Cook House. Site by <a href="https://www.yannyweb.com/" target="_blank" rel="noopener noreferrer">yannyweb.</a>
       </p>
     </Wrapper>
   )
